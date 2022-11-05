601853057b8b75344439e1025addd4f06b05971e
ujjvalporwal76 <100481644+ujjvalporwal76@users.noreply.github.com>
2022-11-06 01:56:38 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <TipsAndUpdatesIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
